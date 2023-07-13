bbbe1debc8a7cc0983c8e23b18c2418579c999de
Diman Rikalov <93448948+dimanrikalov@users.noreply.github.com>
2023-07-13 20:47:02 +0300

    Changed the structure of the ToBuyList component

--- shopping-list-web/src/components/ToBuyList/ToBuyList.tsx
+++ shopping-list-web/src/components/ToBuyList/ToBuyList.tsx
@@ -1,46 +1,38 @@
-import { ROUTES } from '../../router';
-import { Item } from '../utils/Item/Item';
-import { List } from '../utils/List/List';
-import styles from './ToBuyList.module.css';
-import { AiOutlinePlus } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '../utils/Button/Button';
-import { RiEditCircleLine } from 'react-icons/ri';
+import { List } from '../utils/List/List';
+import { Item } from '../utils/Item/Item';
+import styles from './ToBuyList.module.css';
+import { IoIosAddCircle } from 'react-icons/io';
+import { RiEditCircleFill } from 'react-icons/ri';
+import { ROUTES } from '../../router';
 
 export const ToBuyList = () => {
 	const navigate = useNavigate();
 
 	return (
-		<>
-			<div className={styles.operations}>
-				<Button
-					icon={<AiOutlinePlus />}
-					text={'Add'}
-					onClick={() => navigate(ROUTES.ADD_ITEM)}
-				/>
-				<Button
-					icon={<RiEditCircleLine />}
-					text={'Edit'}
-					onClick={() => {}}
-				/>
+		<div className={styles.container}>
+			<div className={styles.operationsContainer}>
+				<button onClick={()=>navigate(ROUTES.ADD_ITEM)}>
+					<IoIosAddCircle />
+				</button>
+				<button>
+					<RiEditCircleFill />
+				</button>
 			</div>
-
 			<div className={styles.listsContainer}>
-				<div className={styles.listContainer}>
-					<h1>To buy</h1>
-					<List>
-						<Item />
-						<Item />
-					</List>
-				</div>
-				<div className={styles.listContainer}>
-					<h1>Already bought</h1>
-					<List>
-						<Item />
-						<Item />
-					</List>
-				</div>
+			<div className={styles.rightSide}>
+				<h4>Items to buy</h4>
+				<List>
+					<Item />
+				</List>
 			</div>
-		</>
+			<div className={styles.rightSide}>
+				<h4>Bought items</h4>
+				<List>
+					<Item />
+				</List>
+			</div>
+			</div>
+		</div>
 	);
 };
