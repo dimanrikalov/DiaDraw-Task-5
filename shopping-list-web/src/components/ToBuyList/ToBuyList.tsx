349ec40d34514605eca1fd79f8208534799b0c90
dimanrikalov <93448948+dimanrikalov@users.noreply.github.com>
2023-07-17 11:39:55 +0300

    Products to buy are now rendered

--- shopping-list-web/src/components/ToBuyList/ToBuyList.tsx
+++ shopping-list-web/src/components/ToBuyList/ToBuyList.tsx
@@ -2,13 +2,30 @@ import { ROUTES } from '../../router';
 import { List } from '../utils/List/List';
 import { Item } from '../utils/Item/Item';
 import styles from './ToBuyList.module.css';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IoIosAddCircle } from 'react-icons/io';
 import { Button } from '../utils/Button/Button';
 import { RiEditCircleFill } from 'react-icons/ri';
+import { DocumentData } from 'firebase/firestore';
+import handleQuery from '../../handles/handleQuery';
+import { COLLECTIONS } from '../../enums/collectionEnums';
 
 export const ToBuyList = () => {
 	const navigate = useNavigate();
+	const [itemsToBuy, setItemsToBuy] = useState<DocumentData[]>([]);
+	useEffect(() => {
+		handleQuery(COLLECTIONS.PRODUCTS_TO_BUY).then(
+			(querySnapshot) => {
+				const res: DocumentData[] = [];
+				querySnapshot.forEach((x) => {
+					res.push(x.data());
+				});
+
+				setItemsToBuy(res);
+			}
+		);
+	}, [setItemsToBuy]);
 
 	return (
 		<div className={styles.container}>
@@ -23,14 +40,23 @@ export const ToBuyList = () => {
 				<div className={styles.listContainer}>
 					<h4>Items to buy</h4>
 					<List>
-						{/* <Item /> */}
+						{itemsToBuy.length > 0 ? (
+						itemsToBuy.map((itemToBuy) => (
+							<Item
+								key={Math.random()}
+								name={itemToBuy.name}
+								price={itemToBuy.price}
+								quantity={itemToBuy.quantity}
+							/>
+						))
+					) : (
+						<h4>List is currently empty</h4>
+					)}
 					</List>
 				</div>
 				<div className={styles.listContainer}>
 					<h4>Bought items</h4>
-					<List>
-						{/* <Item /> */}
-					</List>
+					<List>{/* <Item /> */}</List>
 				</div>
 			</div>
 		</div>
