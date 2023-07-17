bcca94e4911ea2e491a5b312a14789f95c3e558c
dimanrikalov <93448948+dimanrikalov@users.noreply.github.com>
2023-07-17 11:24:38 +0300

    Small modifications

--- shopping-list-web/src/handles/handleQuery.ts
+++ shopping-list-web/src/handles/handleQuery.ts
@@ -1,8 +1,9 @@
 import { firestore } from "../firebase_setup/firebase";
+import { COLLECTIONS } from "../enums/collectionEnums";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
-const handleQuery = async () => {
-    const q = query(collection(firestore, 'products'));
+const handleQuery = async (collectionToQuery: COLLECTIONS) => {
+    const q = query(collection(firestore, collectionToQuery));
     const querySnapshot = await getDocs(q);
     return querySnapshot
 }
