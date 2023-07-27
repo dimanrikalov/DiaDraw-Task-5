e91aff026a1700df989ce935fd6d7c8ab0c68517
dimanrikalov <93448948+dimanrikalov@users.noreply.github.com>
2023-07-27 09:38:37 +0300

    fixed the route guard

--- shopping-list-web/src/router.tsx
+++ shopping-list-web/src/router.tsx
@@ -6,6 +6,7 @@ import { CreateItem } from './components/CreateItem/CreateItem';
 import { SignUp } from './components/SignUp/SignUp';
 import { getAuth } from 'firebase/auth';
 import { useGetUserQuery } from './app/userApi';
+import { SignIn } from './components/SignIn/SignIn';
 
 export enum ROUTES {
 	HOME = '/',
@@ -17,15 +18,28 @@ export enum ROUTES {
 	ALREADY_BOUGHT_ITEMS = '/bought-items',
 }
 
-const AuthGuard = () => {
+enum GUARD_TYPES {
+	USER = 'user',
+	GUEST = 'guest',
+}
+
+const AuthGuard = ({ guardType }: { guardType: GUARD_TYPES }) => {
 	const { data, isLoading } = useGetUserQuery(undefined);
 
 	if (isLoading) {
 		return <h1>Loading...</h1>;
 	}
 
-	if (!data) {
+	if (guardType === GUARD_TYPES.USER) {
+		if (data && 'uid' in data) {
+			return <Outlet />;
+		}
+
 		return <Navigate to={ROUTES.WELCOME} />;
+	}
+
+	if (data && 'uid' in data) {
+		return <Navigate to={ROUTES.HOME} />;
 	}
 	return <Outlet />;
 };
@@ -33,19 +47,24 @@ const AuthGuard = () => {
 export const Router = () => {
 	const router = useRoutes([
 		{
-			path: ROUTES.WELCOME,
-			element: <Welcome />,
+			element: <AuthGuard guardType={GUARD_TYPES.GUEST} />,
+			children: [
+				{
+					path: ROUTES.WELCOME,
+					element: <Welcome />,
+				},
+				{
+					path: ROUTES.SIGN_UP,
+					element: <SignUp />,
+				},
+				{
+					path: ROUTES.SIGN_IN,
+					element: <SignIn />,
+				},
+			],
 		},
 		{
-			path: ROUTES.SIGN_UP,
-			element: <SignUp />,
-		},
-		// {
-		// 	path: ROUTES.SIGN_IN,
-		// 	element: <SignIn />
-		// },
-		{
-			element: <AuthGuard />,
+			element: <AuthGuard guardType={GUARD_TYPES.USER} />,
 			children: [
 				{
 					path: ROUTES.HOME,
